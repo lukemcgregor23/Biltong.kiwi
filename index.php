c0fc6ada6ac2f3f5692159f725c001b7152254c1
BuildTools <unconfigured@null.spigotmc.org>
2016-08-16 13:17:52 +1200

    PHP Test

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="en-us">
-  <head>
-    <title>Biltong.kiwi</title>
-  </head>
-  <body>
-	<nav>
-	</nav>
-  </body>
+	<head>
+		<title>Biltong.kiwi</title>
+	</head>
+	<body>
+		<nav>
+		</nav>
+		<?php
+			echo "Hello test";
+		?>
+	</body>
 </html>
