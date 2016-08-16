3088a6c7cdfb64c01329ca907dd431f7584018fe
BuildTools <unconfigured@null.spigotmc.org>
2016-08-17 08:57:18 +1200

    home sturcture built

---
+++ Home/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en-us">
+	<head>
+		<title>Biltong.kiwi</title>
+		<link rel="stylesheet" type="text/css" href="/src/css/style.css">
+		<?php require $_SERVER['DOCUMENT_ROOT'].'/src/scripts/php.php'; ?>
+	</head>
+	<body>
+		<nav>
+			<?php template('nav.html'); ?>
+		</nav>
+		<content>
+			<br>
+			<br>
+			<br>
+			<br>
+			Content in here
+			<br>
+			<br>
+			<br>
+			<br>
+		</content>
+		<footer>
+			<?php template('footer.html'); ?>
+		</footer>
+	</body>
+</html>
---
+++ src/scripts/php.php
@@ -0,0 +1,6 @@
+<?php
+function template($template){
+	require $_SERVER['DOCUMENT_ROOT'].'/src/templates/'.$template;
+}
+
+?>
